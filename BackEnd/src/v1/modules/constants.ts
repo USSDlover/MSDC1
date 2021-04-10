db098f54c772515e9b036478a471b3fef9b5206d
Alireza Razinejad <a.razinejad@gmail.com>
2021-04-10 19:18:03 +0430

    Dockerized the app v1.1.4

--- BackEnd/src/v1/modules/constants.ts
+++ BackEnd/src/v1/modules/constants.ts
@@ -1,8 +1,5 @@
 export const APP_DB =
   process.env.DB_URL ?? 'mongodb://localhost:27017/soldiering';
-
-// To access MongoDb from docker internal virtual network
-// export const APP_DB = 'mongodb://mongo:27017/soldiering';
 export const APP_COLLECTIONS = {
   exam: 'exams',
   question: 'questions',
