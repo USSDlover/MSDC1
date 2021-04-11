95ecee53032bb04f92c5be078ac489188b6df7f3
Alireza Razinejad <a.razinejad@gmail.com>
2021-04-11 09:32:02 +0430

    Initialized side menu content v0.0.3

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -6,13 +6,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Exam', url: '/folder/Exam', icon: 'book' },
+    { title: 'Testers', url: '/folder/Testers', icon: 'id-card' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public otherPages = [
+    { title: 'Settings', url: '/folder/Settings', icon: 'construct' },
+    { title: 'Logout', url: '/folder/Logout', icon: 'exit' },
+  ];
   constructor() {}
 }
